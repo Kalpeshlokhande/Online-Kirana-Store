050e18dae8df08ebf419b5cae9e6cfd8bf5d569d
Kalpesh <kalpeshlokhande0355@gmail.com>
2025-07-06 14:11:12 +0530

    Order Model Creation

--- app/orders/models.py
+++ app/orders/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from app.products.models import Product
 
 # Create your models here.
+
+ORDER_STATUS=(
+    ('Pending','Pending'),
+    ('Delivered','Delivered'),
+
+)
+
+class Order(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    address=models.TextField()
+    status=models.CharField(max_length=20, choices=ORDER_STATUS, default='Pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class OrderItem(models.Model):
+    order=models.ForeignKey(Order,on_delete=models.CASCADE,related_name='items')
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField()
+    price=models.DecimalField(max_digits=10, decimal_places=2)
+
+    
