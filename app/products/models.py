6d6fab1d2635fbe3771af3f6f3a6028a2b785d98
Kalpesh <kalpeshlokhande0355@gmail.com>
2025-07-06 13:48:52 +0530

    Product Model Creation

---
+++ app/products/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name=models.CharField(max_length=225)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    price=models.DecimalField(max_digits=10,decimal_places=2)
+    description=models.TextField(blank=True)
+    image=models.ImageField(upload_to='product_images/', blank=True,null=True)
+    stock_quantity=models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
--- app/users/models.py
+++ app/users/models.py
@@ -6,6 +6,7 @@ class UserProfile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     phone=models.CharField(max_length=20)
     address=models.TextField(blank=True)
+    is_admin=models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
