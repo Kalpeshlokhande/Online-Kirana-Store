c0dcefa323a77fab0122c001e460333fd5935e35
Kalpesh <kalpeshlokhande0355@gmail.com>
2025-07-10 13:58:57 +0530

    Added Templates for admin

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from config import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,6 @@ urlpatterns = [
     path('api/', include('apps.cart.urls')),
     path('api/', include('apps.orders.urls')),
     path('api/', include('apps.addresses.urls')),
-]
+    path('admin-panel/', include('apps.admin_panel.urls')),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
